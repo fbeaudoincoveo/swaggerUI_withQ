77e41d18f6d7cdd971833dbbeeaaea230d449b4a
fbeaudoin <fbeaudoin@coveo.com>
2017-01-16 13:07:33 -0500

    Added the possibility to use this repository as a testing environment for displaying custom JSON Swagger Files in the SwaggerUI site.

---
+++ README.ml
@@ -0,0 +1,33 @@
+#Coveo SwaggerUI Doc Testing Environment
+
+This can be used to test the display of custom Swagger files in the SwaggerUI generated site.
+
+##Testing With Custom JSON Swagger Files
+
+In the `./docs.html` file, set the `TESTING_LOCALLY` variable to `true` (otherwise, the site will be generated from the
+actual JSON Swagger files hosted on `https://platform.coveo.com/api-docs/`).
+
+If you want to test from locally stored custom JSON Swagger files, you can simply overwrite the example files in the
+`./api-docs-tests/` folder of your local copy of the repository.
+
+If you would rather test from remotely stored custom JSON Swagger files or from JSON Swagger files stored under another
+local path, then you can easily change the TEST_CUSTOM_PATH and/or TEST_CUSTOM_HOST to suit your needs.
+
+In any case, the custom JSON Swagger files names have to map correctly to the original APIs. Thus, files with the
+following names can very easily be tested:
+
+- Platform.json
+- Index.json
+- SecurityCache.json
+- Source.json
+- Activity.json
+- Privilege.json
+- UsageAnalytics.json
+- PushApi.json
+- Extension.json
+
+If you want to test other custom files, you simply have to add them manually in the `./docs.html` file:
+
+```javascript
+addApi('New API Name', PLATFORM_HOST + API_DOCS_PREFIX + 'NewApiName' + TEST_EXTENSION);
+```
